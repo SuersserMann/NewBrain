833e0fc22cfaabd56fbec0c0bed8a745c73c0b44
“SuesserMann” <136633606@qq.com>
2023-12-19 09:35:14 +0800

    2023/12/19

--- 笔试/学习/排列数字.py
+++ 笔试/学习/排列数字.py
@@ -1,20 +1,7 @@
 from itertools import permutations
 
-def get_permutations(nums):
-    # 使用itertools库的permutations函数生成所有排列
-    permutations_list = permutations(nums)
+nums = [1, 1, 0]
+permutations_list = permutations(nums)
+for i in permutations_list:
+    print(list(i))
 
-    # 将每个排列转换为整数并添加到结果集中
-    result = []
-    for p in permutations_list:
-        num_str = ''.join(map(str, p))
-        result.append(int(num_str))
-
-    return result
-
-
-# 示例输入
-nums = [1, 2, 3]
-result = get_permutations(nums)
-print(result)
-print()
