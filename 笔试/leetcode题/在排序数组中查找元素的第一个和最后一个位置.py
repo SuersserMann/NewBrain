56770327ac11f05c180529aa07425e4f829d2e56
“SuesserMann” <136633606@qq.com>
2023-12-23 08:56:44 +0800

    2023/12/23

--- 笔试/leetcode题/在排序数组中查找元素的第一个和最后一个位置.py
+++ 笔试/leetcode题/在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,25 +1,50 @@
 class Solution:
+    # def searchRange(self, nums, target):
+    #     l = 0
+    #     r = len(nums) - 1
+    #     res = []
+    #     while l <= r:
+    #         mid = (l + r) // 2
+    #         if nums[mid] == target:
+    #             mid_l, mid_r = mid - 1, mid + 1
+    #             res.append(mid)
+    #             while mid_l >= 0 and nums[mid_l] == target:
+    #                 res.append(mid_l)
+    #                 mid_l -= 1
+    #             while len(nums) - 1 >= mid_r and nums[mid_r] == target:
+    #                 res.append(mid_r)
+    #                 mid_r += 1
+    #             return [min(res), max(res)]
+    #         if nums[mid] > target:
+    #             r = mid - 1
+    #         else:
+    #             l = mid + 1
+    #     return [-1, -1]
+
     def searchRange(self, nums, target):
+        if not nums:
+            return [-1,-1]
         l = 0
         r = len(nums) - 1
-        res = []
+        mid = (l + r) // 2
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] == target:
-                mid_l, mid_r = mid - 1, mid + 1
-                res.append(mid)
-                while mid_l >= 0 and nums[mid_l] == target:
-                    res.append(mid_l)
-                    mid_l -= 1
-                while len(nums) - 1 >= mid_r and nums[mid_r] == target:
-                    res.append(mid_r)
-                    mid_r += 1
-                return [min(res), max(res)]
-            if nums[mid] > target:
+                break
+            elif nums[mid] > target:
                 r = mid - 1
             else:
                 l = mid + 1
-        return [-1, -1]
+        if nums[mid] == target:
+            l = mid
+            r = mid
+            while l - 1 >= 0 and nums[l - 1] == target:
+                l -= 1
+            while len(nums) - 2 - r >= 0 and nums[r+1]==target:
+                r += 1
+            return [l, r]
+        else:
+            return [-1, -1]
 
 
 s = Solution()
--- 笔试/leetcode题/组合总和.py
+++ 笔试/leetcode题/组合总和.py
@@ -1,20 +1,45 @@
 class Solution:
+    # def combinationSum(self, candidates, target):
+    #     res = []
+    #
+    #     def dfs(amount, path):
+    #         for i in candidates:
+    #             if amount == target:
+    #                 a = sorted(path)
+    #                 if a not in res:
+    #                     res.append(a)
+    #                 return
+    #             elif amount > target:
+    #                 return
+    #             else:
+    #                 path.append(i)
+    #                 dfs(amount + i, path)
+    #                 path.remove(i)
+    #
+    #     dfs(0, [])
+    #     return res
+
     def combinationSum(self, candidates, target):
         res = []
+        n = len(candidates)
 
         def dfs(amount, path):
-            for i in candidates:
-                if amount == target:
-                    a = sorted(path)
-                    if a not in res:
-                        res.append(a)
-                    return
-                elif amount > target:
+            if amount == target:
+                xx = path.copy()
+                xx.sort()
+                if xx not in res:
+                    res.append(xx)
                     return
                 else:
-                    path.append(i)
-                    dfs(amount + i, path)
-                    path.remove(i)
+                    return
+            elif amount > target:
+                return
+
+            for i in range(n):
+                # if candidates[i] not in path:
+                path.append(candidates[i])
+                dfs(amount + candidates[i], path)
+                path.remove(candidates[i])
 
         dfs(0, [])
         return res
